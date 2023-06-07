acc69aeb1f2c3dba45ecae3e237931e2538a6b60
Tavary <50634501+Tavary@users.noreply.github.com>
2023-06-07 15:45:33 +0200

    Fixed it. Ist aber noch keine Funktion

--- Profil_Kruemmung.m
+++ Profil_Kruemmung.m
@@ -1,25 +1,22 @@
 clear
 Profil = load('S801.txt');
 
-t = 4;                          % Tiefe der Profilsehne (wird irgendwie über Schmitz gegeben sein) 
-Au_P = 2/3;                     % Auffädelungspunkt (wird vom User bestimmt)
+t = 10;                          % Tiefe der Profilsehne (wird irgendwie über Schmitz gegeben sein) 
+Au_P = 1/3;                     % Auffädelungspunkt (wird vom User bestimmt)
 N = size(Profil, 1);            % Anzahl Punkte von geometrischen Daten
-r_0 = 5;                       % Abstand Profil zu Nabe    
+r_0 = 10;                       % Abstand Profil zu Nabe    
 
 
 for i = 1:N
-    Profil(i,1) = Profil(i,1) - Au_P;
-    Profil(i,:) = Profil(i,:) * t;
-    Profil(i,3) = r_0;
-    alpha = atand(Profil(i,1)/r_0);
-    c = sqrt(r_0^2 + Profil(i,1)^2) - r_0;
-    Profil(i,1) = Profil(i,1) - c*sind(alpha);
-    Profil(i,3) = r_0 - c*cosd(alpha);
+    Profil(i,1) = Profil(i,1) - Au_P;           % Verschiebung in der x-Komponente auf den Auffädelungspunkt
+    Profil(i,:) = Profil(i,:) * t;              % Skalierung mit der Profilbreite t
+    Profil(i,3) = r_0;                          % Das Profil wird auf den richtigen Abstand zur Nabe gesetzt
+    alpha = (Profil(i,1)/r_0) * (180/pi);       % Berechnung des Winkels des Kreisabschnittes von dem Punkt
+    Profil(i,1) = r_0 * sind(alpha);            % Bestimmen von neuem x und
+    Profil(i,3) = r_0 * cosd(alpha);            % z für die Krümmung des Profils
 end
 
 
 plot3(Profil(:,1), Profil(:,2), Profil(:,3))
 
-xlim([-3 3])
-ylim([-3 3])
-zlim([-3 3])
+axis equal
